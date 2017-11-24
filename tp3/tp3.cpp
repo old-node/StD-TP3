5cb31d6d22df7b21b7fbecbe0ca19edf6856fa35
old-node <olivier.ld@gmail.com>
2017-11-24 12:11:53 -0500

    rebase sql

--- tp3/tp3.cpp
+++ tp3/tp3.cpp
@@ -8,15 +8,38 @@ Description:	3e travail pratique du cours Structure de donnée. Application de de
 
 #include <locale>
 #include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
 using namespace std;
 #include <SFML/Graphics.hpp>
-using namespace sf;
 #include "cursor.h"
+using namespace sf;
+
+
+
+
+#define SQL_RESULT_LEN 240
+#define SQL_RETURN_CODE_LEN 1000
+
+//Variable SQL
+SQLHANDLE sqlConnHandle;
+SQLHANDLE sqlStmtHandle;
+SQLHANDLE sqlEnvHandle;
+SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
 
 
 const float SCREENW = 800;
 const int NBBUTTON = 5;
 const float BW = SCREENW / NBBUTTON;
+
+
+void connexion();
+void deconnexion();
+void selectUsager();
+void ajouteUsager(char *nom, char *prenom);
+
 
 
 // Programme principal
@@ -154,6 +177,165 @@ int main()
 
 	return 0;
 }
+
+
+void connexion() {
+	//Initialisations
+	sqlConnHandle = NULL;
+	sqlStmtHandle = NULL;
+
+
+	try
+	{
+		//Allocations
+		if (!((SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle)) 
+			|| (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)) 
+			|| (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle)))) {
+			//Affichage
+			cout << "Tentative de connexion";
+			cout << "\n";
+
+			//Connexion au SQL Server
+			switch (SQLDriverConnect(sqlConnHandle, NULL,
+				(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=demo;UID=sa;PWD=user123;", 
+				SQL_NTS, retconstring, 1024, NULL, SQL_DRIVER_NOPROMPT)) {
+
+			case SQL_SUCCESS:
+				cout << "Connexion reussi";
+				cout << "\n";
+
+
+				break;
+			case SQL_SUCCESS_WITH_INFO:
+				cout << "Connexion reussi";
+				cout << "\n";
+
+				break;
+			case SQL_INVALID_HANDLE:
+				throw string("Erreur de connexion");
+
+			case SQL_ERROR:
+				throw string("Erreur de connexion");
+			default:
+				throw string("Erreur");
+				break;
+			}
+
+			//Si la connexion est impossible
+			if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
+				throw string("Connexion impossible");
+		}
+	}
+	catch (string const& e)
+	{
+		cout << e << "\n";
+		deconnexion();
+
+		cout << "\n Appuyer sur Retour pour fermer ...";
+		getchar();
+		exit(1);
+	}
+
+}
+
+void deconnexion() {
+
+	cout << "Deconnexion ..." << "\n";
+
+	SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
+	SQLDisconnect(sqlConnHandle);
+	SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
+	SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
+}
+
+void selectUsager() {
+
+	try
+	{
+		connexion();
+
+		//S'il y a un problème avec la requête on quitte l'application sinon on affiche le résultat
+		if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, 
+			(SQLWCHAR*)L"SELECT usagerprenom, usagernom FROM usager", SQL_NTS)) {
+			throw string("Erreur dans la requête");
+		}
+		else {
+			//Déclarer les variables d'affichage
+
+			SQLCHAR nom[SQL_RESULT_LEN];
+			SQLINTEGER ptrnom;
+
+			SQLCHAR prenom[SQL_RESULT_LEN];
+			SQLINTEGER ptrprenom;
+
+			while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+				SQLGetData(sqlStmtHandle, 1, SQL_CHAR, prenom, SQL_RESULT_LEN, &ptrprenom);
+				SQLGetData(sqlStmtHandle, 2, SQL_CHAR, nom, SQL_RESULT_LEN, &ptrnom);
+
+				//Afficher le résultat d'une requête			
+				cout << nom << "  " << prenom << endl;
+			}
+		}
+	}
+	catch (string const& e)
+	{
+		cout << e << "\n";
+		deconnexion();
+	}
+
+	deconnexion();
+}
+
+
+
+void ajouteUsager(char *nom, char *prenom) {
+
+	try
+	{
+		connexion();
+
+		SQLRETURN retcode;
+
+		/*
+		Paramètre SQLBindParameter:
+		- Handler de la requête
+		- No du paramètre (commence à 1)
+		- Est-ce un paramètre de type Input ou Output
+		- Quel est le type de la variable en C++
+		- Quel est le type de la variable en SQL
+		- Quelle est la taille de la colonne dans la BD
+		- Nombre de décimal
+		- Quelle variable ou données (pointeur)
+		- Longueur du buffer
+		- Pointeur du buffer
+		*/
+		retcode = SQLBindParameter(sqlStmtHandle, 1, SQL_PARAM_INPUT, 
+			SQL_C_CHAR, SQL_CHAR, 50, 0, prenom, 0, 0);
+		retcode = SQLBindParameter(sqlStmtHandle, 2, SQL_PARAM_INPUT, 
+			SQL_C_CHAR, SQL_CHAR, 50, 0, nom, 0, 0);
+
+		retcode = SQLPrepare(sqlStmtHandle, 
+			(SQLWCHAR*)L"INSERT INTO Usager (usagerprenom, usagernom) VALUES (?, ?)", SQL_NTS);
+
+		retcode = SQLExecute(sqlStmtHandle);
+
+		if (SQL_SUCCESS != retcode) {
+			throw string("Erreur dans la requête");
+		}
+	}
+	catch (string const& e)
+	{
+		cout << e << "\n";
+
+		deconnexion();
+
+		getchar();
+		exit(1);
+	}
+
+	deconnexion();
+}
+
 
 /*	TP3
 ============
