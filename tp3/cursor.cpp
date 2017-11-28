046970d85b58b186fe49ad9671cd21774d3d3b65
old-node <olivier.ld@gmail.com>
2017-11-28 16:01:18 -0500

    into .cpp

--- tp3/cursor.cpp
+++ tp3/cursor.cpp
@@ -81,6 +81,15 @@ void cursor::initFocus(Vector2f current)
 	_mode->scaleFocus(_click - current);
 }
 
+inline void cursor::setFocus(Vector2f current)
+{
+	if (_mode != nullptr)
+	{
+
+		_mode->scaleFocus(_click - current);
+	}
+}
+
 
 /// Clicker
 void cursor::click(Vector2i click)
@@ -147,21 +156,69 @@ int cursor::unclick(Vector2i current)
 
 /// Getteurs
 
+
+/// Getteurs
+
+inline bool cursor::isClicking(Mouse::Button it)
+{
+	return (isButtonPressed(it) && _clicking);
+}
+
 bool cursor::getClicking() const { return _clicking; }
 
 oButton * cursor::getMode() const { return _mode; }
+
+// Retourner un int au lieu ?*
+
+inline RectangleShape * cursor::getFocus() const
+{
+	assert(_mode != nullptr);
+	return _mode->getFocus();
+}
 
 Vector2f cursor::getClick() const { return _click; }
 
 Vector2f cursor::getCurrent() const { return _current; }
 
+/// Affichage
+// Procède à une méthode avec chaqu'un des boutons
+
+inline void cursor::drawMenu()
+{
+	for (auto & b : _bOptions)	// Pour chaque bouton d'options
+	{
+		if (b->gotMouse(_w)) /// b->gotMouse(_window)
+		{
+			drawButton(b);
+			cursorPos(b, _w);
+		}
+	}
+}
+
 
 /// Affichage
+
+inline void cursor::drawButton(oButton * oB)
+{
+	_w.draw(oB->body());
+	_w.draw(oB->text());
+
+}
 
 void cursor::drawFocus()
 {
 	if (_mode != nullptr)
 		_w.draw(_mode->focus());
 
+}
+
+inline bool cursor::onZone(rRegion z)
+{
+	if (_zones[z].contains((Vector2f)getPosition(_w)))
+	{
+		_zone = z;
+		return true;
+	}
+	return false;
 }
 
